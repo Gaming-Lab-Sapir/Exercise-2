9d70d2fe12ead62bd7b214fc47cd8f48e9532422
Eli_Alhazov <elialhazov20@gmail.com>
2025-09-06 21:22:55 +0300

    enemy spawner + basic enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,21 @@
-using System;
+﻿using System;
 using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
- 
     [SerializeField] float moveSpeed = 2f;
     [SerializeField] float destroyDelay = 0.5f;
     [SerializeField] float bulletHitDelay = 0.05f;
     private bool isActive = true;
     Rigidbody2D rb;
     Animator animator;
+
+    public event Action OnEnemyDestroyed;
+
+    private ScoreManager scoreManager;
+
+    public void Init(ScoreManager sm) => scoreManager = sm;
 
     void Awake()
     {
@@ -22,7 +27,7 @@ public class Enemy : MonoBehaviour
     {
         if (isActive)
         {
-            rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);//goes only left
+            rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y); // goes only left
             if (animator != null) animator.SetBool("IsWalking", true);
         }
         else
@@ -36,14 +41,19 @@ public class Enemy : MonoBehaviour
     {
         if (!isActive) return;
         isActive = false;
+
+        if (scoreManager != null) scoreManager.AddPoint();
+
         StartCoroutine(WaitToDestroy(bulletHitDelay));
     }
 
-
-    void OnCollisionEnter2D(Collision2D other)//eli later if you gonna do "life" for player you can do: life-=1 here;
+    void OnCollisionEnter2D(Collision2D other) 
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            var hp = other.gameObject.GetComponent<PlayerHealth>();
+            if (hp != null) hp.TakeDamage(1);
+
             GetComponent<SpriteRenderer>().color = Color.red;
             StartCoroutine(WaitToDestroy(destroyDelay));
         }
@@ -52,6 +62,7 @@ public class Enemy : MonoBehaviour
     IEnumerator WaitToDestroy(float t)
     {
         yield return new WaitForSeconds(t);
+        OnEnemyDestroyed?.Invoke(); 
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/EnemySpowner.cs
+++ Assets/Scripts/EnemySpowner.cs
@@ -1,16 +1,47 @@
+﻿using System.Collections;
 using UnityEngine;
 
-public class EnemySpowner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject enemyPrefab;    
+    [SerializeField] private float spawnInterval = 3f;  
+    [SerializeField] private int maxEnemies = 10;       
+    [SerializeField] private float spawnRadius = 0.9f;  
+
+    [SerializeField] private ScoreManager scoreManager;
+
+    private int currentEnemies = 0;
+
     void Start()
     {
-        
+        StartCoroutine(SpawnLoop());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnLoop()
     {
-        
+        while (true)
+        {
+            if (currentEnemies < maxEnemies)
+            {
+                Vector2 pos = (Vector2)transform.position + Random.insideUnitCircle * spawnRadius;
+                GameObject e = Instantiate(enemyPrefab, pos, Quaternion.identity);
+
+                Enemy enemy = e.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    enemy.OnEnemyDestroyed += HandleEnemyDestroyed;
+                    enemy.Init(scoreManager); 
+                }
+                currentEnemies++;
+            }
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private void HandleEnemyDestroyed()
+    {
+        currentEnemies--;
+        if (currentEnemies < 0) currentEnemies = 0;
     }
 }
